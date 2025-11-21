4cf960dd72cea3b3f122f4ccc6d9abd8903cc084
Chinmay Garde <chinmaygarde@gmail.com>
2025-11-21 14:32:22 -0800

    Implement file mapping.

--- src/mapping.cc
+++ src/mapping.cc
@@ -1,3 +1,138 @@
 #include "mapping.h"
 
-namespace pack {}
+namespace pack {
+
+Mapping::~Mapping() = default;
+
+uint8_t* FileMapping::GetData() const {
+  return reinterpret_cast<uint8_t*>(handle_.mapping);
+}
+
+uint64_t FileMapping::GetSize() const {
+  return handle_.size;
+}
+
+bool FileMapping::IsValid() const {
+  return !!handle_;
+}
+
+UniqueFD OpenFile(const std::filesystem::path& file_path,
+                  FilePermissions permissions,
+                  Mask<FileFlags> flags,
+                  const std::optional<UniqueFD>& base_directory) {
+  int base_directory_fd = AT_FDCWD;
+  if (base_directory.has_value()) {
+    base_directory_fd = base_directory.value().get();
+  }
+  int oflag = 0;
+  switch (permissions) {
+    case FilePermissions::kReadOnly:
+      oflag |= O_RDONLY;
+      break;
+    case FilePermissions::kWriteOnly:
+      oflag |= O_WRONLY;
+      break;
+    case FilePermissions::kExecuteOnly:
+      oflag |= O_EXEC;
+      break;
+    case FilePermissions::kReadWrite:
+      oflag |= O_RDWR;
+      break;
+  }
+  if (flags & FileFlags::kCreateIfNecessary) {
+    oflag |= O_CREAT;
+  }
+  if (flags & FileFlags::kTruncateToZero) {
+    oflag |= O_TRUNC;
+  }
+  if (flags & FileFlags::kDirectory) {
+    oflag |= O_DIRECTORY;
+  }
+  oflag |= O_CLOEXEC;
+  int fd = PACK_TEMP_FAILURE_RETRY(
+      ::openat(base_directory_fd, file_path.c_str(), oflag));
+  if (fd == -1) {
+    return {};
+  }
+  return UniqueFD{fd};
+}
+
+std::optional<uint64_t> FileGetSize(const UniqueFD& fd) {
+  if (!fd.is_valid()) {
+    return std::nullopt;
+  }
+  struct stat statbuf = {};
+
+  if (::fstat(fd.get(), &statbuf) == -1) {
+    return std::nullopt;
+  }
+
+  return statbuf.st_size;
+}
+
+std::unique_ptr<FileMapping> FileMapping::Create(
+    const UniqueFD& file,
+    size_t mapping_size,
+    size_t mapping_offset,
+    Mask<MappingProtections> protections,
+    MappingModifications mods) {
+  if (!file.is_valid()) {
+    return nullptr;
+  }
+
+  int prot = 0;
+  if (protections & MappingProtections::kNone) {
+    prot |= PROT_NONE;
+  }
+  if (protections & MappingProtections::kRead) {
+    prot |= PROT_READ;
+  }
+  if (protections & MappingProtections::kWrite) {
+    prot |= PROT_WRITE;
+  }
+  if (protections & MappingProtections::kExecute) {
+    prot |= PROT_EXEC;
+  }
+
+  int flags = 0;
+  switch (mods) {
+    case MappingModifications::kPrivate:
+      flags |= MAP_PRIVATE;
+      break;
+    case MappingModifications::kShared:
+      flags |= MAP_SHARED;
+      break;
+  }
+
+  auto mapping =
+      ::mmap(NULL, mapping_size, prot, flags, file.get(), mapping_offset);
+  if (mapping == MAP_FAILED) {
+    return nullptr;
+  }
+  return std::unique_ptr<FileMapping>(
+      new FileMapping(MappingHandle{mapping, mapping_size}));
+}
+
+FileMapping::FileMapping(MappingHandle handle) : handle_(handle) {}
+
+std::unique_ptr<FileMapping> FileMapping::CreateReadOnly(
+    const std::filesystem::path& file_path,
+    const std::optional<UniqueFD>& base_directory) {
+  const auto fd =
+      OpenFile(file_path, FilePermissions::kReadOnly, {}, base_directory);
+  if (!fd.is_valid()) {
+    return nullptr;
+  }
+  const auto size = FileGetSize(fd);
+  if (!size.has_value()) {
+    return nullptr;
+  }
+  return Create(fd,                             //
+                size.value(),                   //
+                0,                              //
+                MappingProtections::kRead,      //
+                MappingModifications::kPrivate  //
+  );
+}
+
+}  // namespace pack
--- src/mapping.h
+++ src/mapping.h
@@ -1,3 +1,140 @@
 #pragma once
 
-namespace pack {}
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <cstdint>
+#include <filesystem>
+#include <memory>
+#include <optional>
+
+#include "mask.h"
+#include "unique_object.h"
+
+namespace pack {
+
+#define PACK_TEMP_FAILURE_RETRY(expression)          \
+  ({                                                 \
+    intptr_t __result;                               \
+    do {                                             \
+      __result = (expression);                       \
+    } while ((__result == -1L) && (errno == EINTR)); \
+    __result;                                        \
+  })
+
+class Mapping {
+ public:
+  virtual uint8_t* GetData() const = 0;
+
+  virtual uint64_t GetSize() const = 0;
+
+  virtual ~Mapping();
+
+  Mapping(const Mapping&) = delete;
+
+  Mapping(Mapping&&) = delete;
+
+  Mapping& operator=(const Mapping&) = delete;
+
+  Mapping& operator=(Mapping&&) = delete;
+
+ protected:
+  Mapping() = default;
+};
+
+struct UniqueFDTraits {
+  static int InvalidValue() { return -1; }
+
+  static bool IsValid(int value) { return value != InvalidValue(); }
+
+  static void Free(int fd) { PACK_TEMP_FAILURE_RETRY(::close(fd)); }
+};
+
+using UniqueFD = UniqueObject<int, UniqueFDTraits>;
+
+struct MappingHandle {
+  void* mapping = MAP_FAILED;
+  size_t size = 0;
+
+  constexpr bool operator==(const MappingHandle& handle) const = default;
+
+  constexpr explicit operator bool() const { return *this == MappingHandle{}; }
+};
+
+struct UniqueMappingHandleTraits {
+  static MappingHandle InvalidValue() { return {}; }
+
+  static bool IsValid(const MappingHandle& value) {
+    return value != InvalidValue();
+  }
+
+  static void Free(const MappingHandle& handle) {
+    ::munmap(handle.mapping, handle.size);
+  }
+};
+
+using UniqueMappingHandle =
+    UniqueObject<MappingHandle, UniqueMappingHandleTraits>;
+
+enum class FilePermissions {
+  kReadOnly,
+  kWriteOnly,
+  kExecuteOnly,
+  kReadWrite,
+};
+
+enum class MappingProtections {
+  kNone = 0 << 0,
+  kRead = 1 << 0,
+  kWrite = 1 << 1,
+  kExecute = 1 << 2,
+};
+PACK_ENUM_IS_MASK(MappingProtections);
+
+enum class MappingModifications {
+  kPrivate,
+  kShared,
+};
+
+enum class FileFlags {
+  kCreateIfNecessary = 1 << 0,
+  kTruncateToZero = 1 << 1,
+  kDirectory = 1 << 2,
+};
+PACK_ENUM_IS_MASK(FileFlags);
+
+UniqueFD OpenFile(const std::filesystem::path& file_path,
+                  FilePermissions permissions,
+                  Mask<FileFlags> flags,
+                  const std::optional<UniqueFD>& base_directory);
+
+std::optional<uint64_t> FileGetSize(const UniqueFD& fd);
+
+class FileMapping final : public Mapping {
+ public:
+  static std::unique_ptr<FileMapping> Create(
+      const UniqueFD& file,
+      size_t mapping_size,
+      size_t mapping_offset,
+      Mask<MappingProtections> protections,
+      MappingModifications mods);
+
+  static std::unique_ptr<FileMapping> CreateReadOnly(
+      const std::filesystem::path& file_path,
+      const std::optional<UniqueFD>& base_directory);
+
+  uint8_t* GetData() const override;
+
+  uint64_t GetSize() const override;
+
+  bool IsValid() const;
+
+ private:
+  MappingHandle handle_;
+
+  FileMapping(MappingHandle handle);
+};
+
+}  // namespace pack
