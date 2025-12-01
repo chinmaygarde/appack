cd1a968416a19a20fafabcae55904b9d6e818785
Chinmay Garde <chinmaygarde@gmail.com>
2025-11-30 16:54:54 -0800

    Stub commands.

--- src/cli.cc
+++ src/cli.cc
@@ -8,8 +8,18 @@ namespace appack {
 bool Main(int argc, char const* argv[]) {
   auto parser = args::ArgumentParser{"Manage packages."};
   args::HelpFlag help(parser, "help", "Display help menu", {'h', "help"});
+
+  // Work on assets.
+  args::Command assets(parser, "assets", "Manage assets in the package.");
+  args::Command add(assets, "add", "Add files and directories to the package.");
+  args::Command list(assets, "list", "List the assets in the package.");
+  args::Command clear(assets, "clear", "Clear all assets in the package.");
+
+  // Installation.
+  args::Command install(parser, "install", "Install the package.");
+
   if (!parser.ParseCLI(argc, argv)) {
-    std::cerr << "Could not parse arguments." << std::endl
+    std::cerr << "Invalid arguments." << std::endl
               << parser.Help() << std::endl;
     return false;
   }
